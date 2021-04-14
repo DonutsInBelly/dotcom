b1fe6ed9a783576650f0700b8e5c09ce66937f8b
Carlin Au <carlinau@gmail.com>
2021-04-13 22:57:14 -0400

    add commands page

---
+++ pages/commands.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@material-ui/core";
+
+import { MongoClient } from "mongodb";
+
+export default function CommandPage({ commands }) {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Command Name</TableCell>
+          <TableCell>Command Response</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {commands.map((row) => {
+          return (
+            <TableRow>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.response}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
+
+export async function getServerSideProps() {
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@twitchbot.ljjwk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  // (err) => {
+  //   console.log(err);
+  //   client.close();
+  // }
+  let commands = [];
+  const collection = await client.db("Commands").collection("custom");
+  await collection
+    .find({}, { name: 1, response: 1, id: 0, _id: 0 })
+    .forEach((element) => {
+      commands.push({
+        name: element.name,
+        response: element.response,
+      });
+    });
+
+  // console.log(await commands.toArray());
+  return {
+    props: {
+      commands: commands,
+    },
+  };
+}
